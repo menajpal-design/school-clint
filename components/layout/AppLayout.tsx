ac85f7fb1690cee25fdfdcd093e5a12e79249c75
Ishan Miya <menajpal@gmail.com>
2026-05-16 15:48:03 +0600

    fix mobile app layout height and overflow

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -12,18 +12,20 @@ export function AppLayout({ children }: AppLayoutProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen flex-col bg-gray-50">
+    <div className="flex min-h-screen flex-col bg-gray-50 mobile-app-layout">
       <Navbar
         onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)}
         isMobileMenuOpen={isSidebarOpen}
       />
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex min-h-0 flex-1 mobile-content-shell">
         <Sidebar
           isOpen={isSidebarOpen}
           onClose={() => setIsSidebarOpen(false)}
         />
-        <main className="min-w-0 flex-1 overflow-y-auto bg-gray-50 p-4 lg:ml-0 lg:p-6">
-          {children}
+        <main className="min-w-0 flex-1 bg-gray-50 p-3 pb-20 md:p-4 lg:ml-0 lg:p-6 mobile-main-content">
+          <div className="mx-auto w-full max-w-[1600px] min-w-0">
+            {children}
+          </div>
         </main>
       </div>
     </div>
