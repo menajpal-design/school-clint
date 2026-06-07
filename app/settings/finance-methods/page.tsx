70aaf6d19609d074061a3096475ab37aead12bf0
Md Hridoy Sheikh <138392668+hd31520@users.noreply.github.com>
2026-06-07 09:40:42 +0600

    Add finance methods settings page

---
+++ app/settings/finance-methods/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { PaymentSettingsPanel } from "../PaymentSettingsPanel";
+
+export default function FinanceMethodsSettingsPage() {
+  return <main className="p-3 md:p-6"><PaymentSettingsPanel /></main>;
+}
